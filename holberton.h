e4e96c638491c1e4a6a277efdddbd32a32f34416
Jacob Biehler <biehlerj@gmail.com>
2017-10-22 06:24:42 +0000

    Commit to update header file and start the printf project

--- holberton.h
+++ holberton.h
@@ -1,6 +1,19 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+
+/**
+ * struct convert - the structure used to convert types for printing
+ * @s: whats to be converted
+ * @print: the function to print
+ * Description: the structure to determine what to print and how
+ */
+typedef struct convert
+{
+	char *s;
+	int *(*print)(va_list, int *);
+} convert_t;
+
 int _printf(const char *format, ...);
 
 #endif /* _HOLBERTON_H_ */
---
+++ jacob.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+ * _printf - recreating printf
+ * @format: the type to be printed
+ * Return: The number of characters printed
+ */
+int _printf(const char *format)
+{
+	int i;
