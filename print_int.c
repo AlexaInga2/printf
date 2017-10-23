cfbb9c3d81c10ea92f38dd3afabcb2a6f5a8ff61
Jacob Biehler <biehlerj@gmail.com>
2017-10-23 05:10:33 +0000

    Commit to change print_int function to conform with structure

--- print_int.c
+++ print_int.c
@@ -5,82 +5,80 @@
 /**
  * print_int - print an integer
  * @i: interger to print
- *
+ * @sum: count of the chars printed
  * Return: sum of intergers printed (chars & digits)
  */
-int print_int(va_list i)
+int *print_int(va_list i, int *sum)
 {
-  int a[10];
-  int max, num, total, count, index;
+	int a[10];
+	int max, num, total, index;
 
   /* translate i to actual arg value */
-  num = va_arg(i, int);
-  count = 0;
-  max = 1000000000; /* int is in base 10 */
-  a[0] = num / max;
+	num = va_arg(i, int);
+	max = 1000000000; /* int is in base 10 */
+	a[0] = num / max;
 
-  for (index = 1; index < 10; index++)
-    {
-      /* approaching like fizzbuzz */
-      max = max / 10;
-      a[index] = (num / max) % 10;
-    }
-  /* check if negative */
-  if (num < 0)
-    {
-      _putchar('-');
-      count++;
-      for (index = 0; index < 10; index++)
-	a[index] = a[index] * -1;
-    }
-  for (index = 0, total = 0; index < 10; index++)
-    {
-      total = total + a[index];
-
-      if (total != 0 || index == 9)
+	for (index = 1; index < 10; index++)
 	{
-	  _putchar('0' + a[index]);
-	  count++;
+	/* approaching like fizzbuzz */
+		max = max / 10;
+		a[index] = (num / max) % 10;
 	}
-    }
-  return (count);
+	/* check if negative */
+	if (num < 0)
+	{
+		_putchar('-');
+		*sum += 1;
+		for (index = 0; index < 10; index++)
+			a[index] = a[index] * -1;
+	}
+	for (index = 0, total = 0; index < 10; index++)
+	{
+		total = total + a[index];
+
+		if (total != 0 || index == 9)
+		{
+			_putchar('0' + a[index]);
+			*sum += 1;
+		}
+	}
+	return (sum);
 }
 
 /**
  * print_dec - print decimal
  * @d: decimal to print
- *
+ * @sum: count of chars printed
  * Return: the sum of printed dec
  */
-int print_dec(va_list d)
+int *print_dec(va_list d, int *sum)
 {
-  int a[10];
-  int max, num, total, count, index;
+	int a[10];
+	int max, num, total, index;
 
-  num = va_arg(d, int);
-  count = 0;
-  max = 1000000000;
-  a[0] = num / max;
-  for (index = 1; index < 10; index++)
-    {
-      max = max / 10;
-      a[index] = (num / max) % 10;
-    }
-  if (num < 0)
-    {
-      _putchar('-');
-      count++;
-      for (index = 0; index < 10; index++)
-	a[index] = a[index] * -1;
-    }
-  for (index = 0, total = 0; index < 10; index++)
-    {
-      total = total + a[index];
-      if (total != 0 || index == 9)
+	num = va_arg(d, int);
+	max = 1000000000;
+	a[0] = num / max;
+	for (index = 1; index < 10; index++)
 	{
-	  _putchar('0' + a[index]);
-	  count++;
+		max = max / 10;
+		a[index] = (num / max) % 10;
 	}
-    }
-  return (count);
+	if (num < 0)
+	{
+		_putchar('-');
+		*sum += 1;
+		for (index = 0; index < 10; index++)
+			a[index] = a[index] * -1;
+	}
+	for (index = 0, total = 0; index < 10; index++)
+	{
+		total = total + a[index];
+		if (total != 0 || index == 9)
+		{
+			_putchar('0' + a[index]);
+			*sum += 1;
+		}
+	}
+	return (sum);
 }
